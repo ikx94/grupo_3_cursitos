4b46615ff1ece57dc36a6be43ee9beca81569a28
Ian <katzian@gmail.com>
2020-09-04 12:43:06 -0300

    Agregue una linea en app.js que arreglo que se puedan cargar los archivos CSS y las imagenes. Modifique las rutas en index.html

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.listen(3000, () => {
     console.log("Escuchando en el puerto 3000");
 });
 
+app.use(express.static(__dirname));
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/src/public/index.html"));
 });
