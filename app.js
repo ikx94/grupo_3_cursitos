6db163f7d70813df69dfd09a25e7f2e225bc61d7
Matias Gonzalo Vivas Castillo <mativivas77@gmail.com>
2020-09-04 17:29:45 -0300

    Agrego estructura inicial de la carpeta Public, junto con el acceso desde el endpoint del server

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ app.use(express.static(__dirname));
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/src/public/index.html"));
 });
+
+app.get("/detalleProducto", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/detalleProducto.html"));
+});
+
+app.get("/carrito", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/carrito.html"));
+});
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/login.html"));
+});
+
+app.get("/registro", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/registro.html"));
+});
