5b510771ed3fd48cfc777e036f71ea8358d08cd9
Ian <katzian@gmail.com>
2020-09-03 21:59:15 -0300

    Modifique estructura

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => { console.log('Escuchando en el puerto 3000');})
+app.listen(3000, () => {
+    console.log("Escuchando en el puerto 3000");
+});
