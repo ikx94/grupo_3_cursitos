43d42df1b335591f4c983c5b25cf1974d5466a67
Ian <katzian@gmail.com>
2020-09-04 17:52:25 -0300

    Agregue BrowserSync para que el navegador se actualice automáticamente cuando cambiamos cualquier archivo

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const path = require("path");
+const browserSync = require("browser-sync");
 
 const app = express();
+const port = 2999;
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Escuchando en el puerto 3000");
+    browserSync({
+        files: ["src/**/*.{html,js,css}"],
+        online: false,
+        open: false,
+        port: port + 1,
+        proxy: "localhost:" + port,
+        ui: false,
+    });
 });
 
 app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/index.html"));
+});
+
+app.get("/index.html", function (req, res) {
     res.sendFile(path.join(__dirname + "/src/public/index.html"));
 });
 
