0020d1a25363732d806d01c8f0e5b45ff4e6dec9
Ian <katzian@gmail.com>
2020-09-03 23:38:48 -0300

    Empezando index.html

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Escuchando en el puerto 3000");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/src/public/index.html"));
 });
